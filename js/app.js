006452324b2f9519d01bcc18c4f7cfc4ffc9711b
Ing. Eduardo E. Enríquez <rekineke@hotmail.com>
2024-07-08 22:20:40 -0500

    Agregando validaciones en los campos del formulario para agregar un nuevo gasto

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ let presupuesto;
 eventListeners();
 function eventListeners() {
   document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+  formulario.addEventListener('submit', agregarGasto);
 }
 
 
@@ -32,6 +33,27 @@ class UI {
     document.querySelector('#total').textContent = presupuesto;
     document.querySelector('#restante').textContent = restante;
   }
+
+  imprimirAlerta(mensaje, tipo) {
+    const divMensaje = document.createElement('div');
+    divMensaje.classList.add('text-center', 'alert');
+
+    if(tipo === 'error') {
+      divMensaje.classList.add('alert-danger');
+    } else { 
+      divMensaje.classList.add('alert-success');
+    }
+    
+    divMensaje.textContent = mensaje;
+
+    //insertar en HTML
+    document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+    //quitar mensaje
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
 }
 
 //Instanciar la clase UI
@@ -52,4 +74,23 @@ function preguntarPresupuesto() {
   console.log(presupuesto);
 
   ui.insertarPresupuesto(presupuesto); //Agrega el presupuesto a la interfaz
+}
+
+//Añade gastos
+
+function agregarGasto(e) {
+  e.preventDefault();
+
+  //Leer datos del formulario
+  const nombreGasto = document.querySelector('#gasto').value;
+  const cantidadGasto = Number(document.querySelector('#cantidad').value);
+
+  //Validar
+  if (nombreGasto === '' || cantidadGasto === '') {
+    ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+    return;
+  } else if (cantidadGasto <= 0 || isNaN(cantidadGasto)) {
+    ui.imprimirAlerta('Cantidad no válida', 'error');
+    return;
+  }
 }
